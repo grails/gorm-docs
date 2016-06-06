784c2814df22ac2cb4548e0acce7c9da68cadca4
graemerocher <graeme.rocher@gmail.com>
2016-06-06 10:01:02 +0200

    Start publishing documentation

--- travis-build.sh
+++ travis-build.sh
@@ -12,8 +12,46 @@ EXIT_STATUS=0
 echo "Publishing archives for branch $TRAVIS_BRANCH"
 if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
-  echo "Publishing Documentation"
+    echo "Publishing Documentation"
+    git config --global user.name "$GIT_NAME"
+    git config --global user.email "$GIT_EMAIL"
+    git config --global credential.helper "store --file=~/.git-credentials"
+    echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
 
+    git clone https://${GH_TOKEN}@github.com/grails/grails-data-mapping.git -b gh-pages gh-pages --single-branch > /dev/null
+    cd gh-pages
+
+    # If this is the master branch then update the snapshot
+    if [[ $TRAVIS_BRANCH == 'master' ]]; then
+        mkdir -p snapshot
+        cp -r ../build/docs/. ./snapshot/
+
+        git add snapshot/*    
+    else 
+        version="$TRAVIS_TAG"
+        version=${version:1}
+
+        mkdir -p latest
+        cp -r ../build/docs/. ./latest/
+        git add latest/*
+
+        majorVersion=${version:0:4}
+        majorVersion="${majorVersion}x"
+
+        mkdir -p "$version"
+        cp -r ../build/docs/. "./$version/"
+        git add "$version/*"
+
+        mkdir -p "$majorVersion"
+        cp -r ../build/docs/. "./$majorVersion/"
+        git add "$majorVersion/*"
+    fi
+
+
+    git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+    git push origin HEAD
+    cd ..
+    rm -rf gh-pages
 
 fi
 
